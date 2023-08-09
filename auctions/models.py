e44369f36e430934f5d09690882be1910d874813
Alyona Dudina <badyukiko@gmail.com>
2023-08-09 19:30:34 +0300

    create a models

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,44 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinValueValidator
+from decimal import Decimal
 
 
 class User(AbstractUser):
     pass
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+
+
+class Listing(models.Model):
+    name = models.CharField(max_length=60)
+    detail = models.TextField(blank=True)
+    start_price = models.DecimalField(decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    date_of_create = models.DateField(auto_now=True)
+    photo = models.ImageField(upload_to='listings/%Y/%m/%d/',
+                              blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, default='unspecified')
+    winner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    cost = models.DecimalField(decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
+
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    detail = models.TextField()
+
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+
+
