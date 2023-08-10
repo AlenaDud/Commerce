9d6dc25c34c85fe409fc549306b46a3c4ad4a120
Alyona Dudina <badyukiko@gmail.com>
2023-08-10 16:27:17 +0300

    correct the admin.py

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Listing, Comments, Bid, Watchlist, Category
+
 
 # Register your models here.
+
+
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = '__all__'
+    list_editable = ['detail', 'start_price', 'is_active',
+                     'photo', 'user', 'category', 'winner']
+
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = '__all__'
+    list_editable = ['listing', 'detail']
+
+
+@admin.register(Bid)
+class BidAdmin(admin.ModelAdmin):
+    list_display = '__all__'
+    list_editable = ['listing', 'cost']
+
+
+@admin.register(Watchlist)
+class WatchlistAdmin(admin.ModelAdmin):
+    list_display = '__all__'
+    list_editable = ['listing']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = '__all__'
+    pass
